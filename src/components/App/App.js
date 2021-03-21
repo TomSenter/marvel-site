16bf7a9cc2192ce9a5305006299c73fd76fe9ed2
TomSenter <trs1@icloud.com>
2021-03-21 11:56:39 +0000

    change paths back

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -53,10 +53,10 @@ class App extends React.Component{
         <div className="App">
           
             <Navbar/>
-            <Route path='https://tomsenter.github.io/marvel-site/'  render={(props)=>(
+            <Route path='/' exact render={(props)=>(
             <Home {...props} search={this.searchCharacter} characters={this.state.characters}/>
              )}/>
-            <Route path='https://tomsenter.github.io/marvel-site/comics/:id/:name' render={(props)=>(
+            <Route path='/comics/:id/:name' render={(props)=>(
             <ComicSet {...props} getComics={this.getComics} comics={this.state.comics}/>
             )}/>
             <Footer/>
